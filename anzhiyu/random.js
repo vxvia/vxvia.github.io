4a99e80982fa39dadfe91a0c17759e0e051eb7e5
vxvia <vxvia@outlook.com>
2025-06-25 18:16:26 +0800

    Site updated: 2025-06-25 18:16:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/25/这是一篇新的博文/"];function toRandomPost(){
+var posts=["undefined/试作/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
