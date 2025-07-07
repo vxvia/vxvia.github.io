2b16a807e05245dbad018383ccc5c67cb5c89db5
vxvia <vxvia@Outlook.com>
2025-07-07 21:54:00 +0800

    Site updated: 2025-07-07 21:53:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/试作/"];function toRandomPost(){
+var posts=["undefined/签名/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
