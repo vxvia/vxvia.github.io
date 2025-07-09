85807702940becfac1a69c0ac1a174ddc26b3fbb
vxvia <vxvia@Outlook.com>
2025-07-09 19:46:53 +0800

    Site updated: 2025-07-09 19:46:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/你好/"];function toRandomPost(){
+var posts=["undefined/网络/分享/关于分享/","undefined/文章/查找游戏/","undefined/文章/你好/","undefined/网络/工具/虚空终端使用教程/","undefined/游戏/RPG/萨布丽娜和无力的灵魂/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
